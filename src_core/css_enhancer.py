a745858078eac9ffc40db7435443280e0d92d968
null <48532067+null138@users.noreply.github.com>
2026-01-23 00:16:03 +0400

    1.1release
    Files changed

--- src_core/css_enhancer.py
+++ src_core/css_enhancer.py
@@ -15,7 +15,7 @@ init(autoreset=True)
 
 # update reminder. do not change this link
 GITHUB_REPO = "null138/source-engine-css-enhancer"
-CSS_ENHANCER_VERSION = "1.0beta"
+CSS_ENHANCER_VERSION = "1.1release"
 
 CONFIG_PATH = Path("css_enhancer_config.json")
 MODULE_FOLDER = Path("modules")
